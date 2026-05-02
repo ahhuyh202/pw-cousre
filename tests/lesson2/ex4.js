fade690ef32d74c796c6e71bc5e27e2ae38e1ec3
Minh Anh <mia.huyh1408@gmail.com>
2026-05-02 16:16:29 +0700

    bo sung ex4

---
+++ tests/lesson2/ex4.js
@@ -0,0 +1,6 @@
+const chieucao = 152;
+const cannanglytuong = 52 * 9 / 10;
+const muccantoida = 52;
+const muccantoithieu = 52 * 8 / 10;
+
+console.log(`${cannanglytuong}kg`, `${muccantoida}kg`, `${muccantoithieu}kg`);
